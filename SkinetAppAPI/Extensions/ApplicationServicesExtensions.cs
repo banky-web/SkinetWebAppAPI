80913fac17af1339b70fca5230c0b43e6a9834d1
Banky77 <bankolevictor77@gmail.com>
2023-03-20 23:37:32 +0100

    add Redis for basket shopping storage

--- SkinetAppAPI/Extensions/ApplicationServicesExtensions.cs
+++ SkinetAppAPI/Extensions/ApplicationServicesExtensions.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 using SkinetAppAPI.Errors;
 
+using StackExchange.Redis;
+
 namespace SkinetAppAPI.Extensions
 {
     public static class ApplicationServicesExtensions
@@ -17,9 +19,17 @@ namespace SkinetAppAPI.Extensions
             services.AddSwaggerGen();
 
             services.AddDbContext<StoreContext>(options =>
-                       options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+             });
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });  
 
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ApiBehaviorOptions>(options =>
